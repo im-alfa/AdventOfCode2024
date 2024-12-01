6f62a347b3fda501dedf3b478dc42a5f2a04d828
alfa <alfamodz26@gmail.com>
2024-12-01 16:19:22 +0000

    feat: optimize day1a

--- crates/day1a/src/main.rs
+++ crates/day1a/src/main.rs
@@ -2,26 +2,22 @@ use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
+    let (mut col1, mut col2) = (Vec::with_capacity(1000), Vec::with_capacity(1000));
 
-    let (mut col1, mut col2): (Vec<_>, Vec<_>) = include_str!("input.txt")
-        .lines()
-        .map(|l| {
-            let mut iter = l.split("   ");
-            (
-                iter.next().unwrap().parse::<i32>().unwrap(),
-                iter.next().unwrap().parse::<i32>().unwrap(),
-            )
-        })
-        .unzip();
+    include_str!("input.txt").lines().for_each(|l| {
+        let mut iter = l.split("   ");
+        col1.push(iter.next().unwrap().parse::<u32>().unwrap());
+        col2.push(iter.next().unwrap().parse::<u32>().unwrap());
+    });
 
-    col1.sort();
-    col2.sort();
+    col1.sort_unstable();
+    col2.sort_unstable();
 
     let ans = col2
         .iter()
-        .zip(col1.iter())
-        .map(|a| (a.0 - a.1).abs())
-        .sum::<i32>();
+        .zip(col1)
+        .map(|(a, b)| a.abs_diff(b))
+        .sum::<u32>();
 
     let end = start.elapsed();
 
