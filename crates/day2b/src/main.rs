63ae9c0740f583943326450358131a60389ed5d7
alfa <alfamodz26@gmail.com>
2024-12-02 19:30:10 +0000

    feat: implement day2b

---
+++ crates/day2b/src/main.rs
@@ -0,0 +1,48 @@
+use std::time::Instant;
+
+fn is_valid_trend(nums: &[isize]) -> bool {
+    (0..nums.len()).any(|i| {
+        nums[0..i]
+            .iter()
+            .chain(&nums[i + 1..]) // create a slice without the element at index `i`
+            .collect::<Vec<_>>()
+            .windows(2)
+            .try_fold(0, |dir, window| {
+                let (a, b) = (window[0], window[1]);
+
+                if dir >= 0 && (1..=3).contains(&(b - a)) {
+                    Ok(1)
+                } else if dir <= 0 && (1..=3).contains(&(a - b)) {
+                    Ok(-1)
+                } else {
+                    Err(())
+                }
+            })
+            .is_ok()
+    })
+}
+
+fn main() {
+    let now = Instant::now();
+
+    let ans = include_bytes!("input.txt")
+        .split(|&c| c == b'\n')
+        .filter_map(|line| {
+            if line.is_empty() {
+                return None;
+            }
+
+            Some(
+                line.split(|&c| c == b' ')
+                    .map(|num| atoi::atoi::<isize>(num).unwrap())
+                    .collect::<Vec<isize>>(),
+            )
+        })
+        .filter(|nums| is_valid_trend(nums))
+        .count();
+
+    let elapsed = now.elapsed();
+
+    println!("{:?}", ans);
+    println!("{:?}", elapsed);
+}
