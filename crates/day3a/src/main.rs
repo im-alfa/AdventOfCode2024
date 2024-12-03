b93928f272eaeb2857c2ae1ff082c17781dcf445
alfa <alfamodz26@gmail.com>
2024-12-03 18:04:04 +0000

    feat: implement day3a

---
+++ crates/day3a/src/main.rs
@@ -0,0 +1,21 @@
+use std::time::Instant;
+
+use regex::bytes::Regex;
+
+fn main() {
+    let start = Instant::now();
+
+    let rx = Regex::new(r"mul\((\d*),(\d*)\)").unwrap();
+
+    let ans = rx
+        .captures_iter(include_bytes!("input_test.txt"))
+        .map(|c| {
+            atoi::atoi::<usize>(c.get(1).unwrap().as_bytes()).unwrap()
+                * atoi::atoi::<usize>(c.get(2).unwrap().as_bytes()).unwrap()
+        })
+        .sum::<usize>();
+
+    let elapsed = start.elapsed();
+
+    dbg!(ans, elapsed);
+}
