e6fe2a9978aa2c812a501cfed06aa038b8dcd6a6
alfa <alfamodz26@gmail.com>
2024-12-07 18:36:00 +0000

    feat: implement day7a

---
+++ crates/day7a/src/main.rs
@@ -0,0 +1,36 @@
+use std::time::Instant;
+
+use rayon::prelude::*;
+
+fn solve_dfs(terms: &[usize], prev: usize, res: usize) -> bool {
+    if terms.len() == 0 || prev > res {
+        return prev == res;
+    }
+    solve_dfs(&terms[1..], terms[0] * prev, res) || solve_dfs(&terms[1..], terms[0] + prev, res)
+}
+
+fn main() {
+    let start = Instant::now();
+
+    let input = include_bytes!("input.txt")
+        .split(|&b| b == b'\n')
+        .par_bridge()
+        .map(|l| {
+            let column_pos = l.iter().position(|&b| b == b':').unwrap();
+            let res = atoi::atoi::<usize>(&l[0..column_pos]).unwrap();
+            let terms: Vec<_> = l[column_pos + 2..]
+                .split(|&b| b == b' ')
+                .map(|n| atoi::atoi::<usize>(n).unwrap())
+                .collect();
+
+            match solve_dfs(&terms[1..], terms[0], res) {
+                true => res,
+                false => 0,
+            }
+        })
+        .sum::<usize>();
+
+    let elapsed = start.elapsed();
+
+    dbg!(elapsed, input);
+}
